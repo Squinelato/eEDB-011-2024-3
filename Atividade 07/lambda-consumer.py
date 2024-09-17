af3ef1b6bb7ee372ca45ce93120ba1f6bd0cd147
Marcelo Pena <marcelo.pena@viaconsulting.com.br>
2024-09-16 23:27:49 -0300

    Update files

--- Atividade 07/lambda-consumer.py
+++ Atividade 07/lambda-consumer.py
@@ -1,2 +1,64 @@
+import awswrangler as wr
+import pandas as pd
+import boto3
+
+sqs = boto3.client('sqs')
+glue = boto3.client('glue')
+
 def lambda_handler(event, context):
-    print(event)
+    try:
+        print(event)
+        
+        messages = list()
+        for record in event['Records']:
+            body = record['body'].encode('latin1').decode('latin1').split(';')
+            messages.append(body)
+            
+        print(messages)
+        
+        df = pd.DataFrame(messages, columns=["Ano", 
+                                             "Trimestre", 
+                                             "Categoria", 
+                                             "Tipo", 
+                                             "CNPJ IF", 
+                                             "Instituição financeira", 
+                                             "Índice", 
+                                             "Quantidade de reclamações reguladas procedentes", 
+                                             "Quantidade de reclamações reguladas - outras", 
+                                             "Quantidade de reclamações não reguladas", 
+                                             "Quantidade total de reclamações", 
+                                             "Quantidade total de clientes - CCS e SCR", 
+                                             "Quantidade de clientes - CCS", 
+                                             "Quantidade de clientes - SCR", 
+                                             "_14"])
+        
+        df.drop(columns=['_14'], inplace=True)
+        df['CNPJ IF'] = df['CNPJ IF'].str.replace(" ", "")
+        df["CNPJ IF"] = df["CNPJ IF"].map(lambda cnpj: cnpj.zfill(8) if len(cnpj) > 0 and len(cnpj) < 8 else cnpj)
+        
+        glue_database = "trzd"
+        glue_table = "banks"
+        df_glue = wr.athena.read_sql_table(table=glue_table, database=glue_database)
+
+        df_merged = pd.merge(df, df_glue, left_on='CNPJ IF', right_on='cnpj', how='left')
+        df_merged.drop(columns=['cnpj','sk_cnpj_segment','filename'], inplace=True)
+
+        target_database = 'rwzd'
+        target_table = 'enriched_claims'
+        s3_output_path = f's3://eedb011-2024-g2-337171436930-raw/{target_database}/'
+
+        wr.s3.to_parquet(
+            df=df_merged,
+            path=s3_output_path,
+            database=target_database,
+            table=target_table,
+            dataset=True,
+            mode='append',
+            dtype={ column: 'string' for column in df_merged.columns },
+            compression='snappy'
+        )
+
+        print('End')
+
+    except Exception as e:
+        print(f"Erro: {str(e)}")
--- Atividade 07/lambda-producer.py
+++ Atividade 07/lambda-producer.py
@@ -1,24 +1,31 @@
 import os
 import csv
-import time
 import boto3
-import random
 
 s3 = boto3.client('s3')
 sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    csv_key = event['Records'][0]['s3']['object']['key']
-    queue_url = os.environ['SQS_QUEUE_URL']
-
-    response = s3.get_object(Bucket=bucket, Key=csv_key)
-    lines = response['Body'].read().decode('utf-8').splitlines()
+    try:
+        print(event)
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        csv_key = event['Records'][0]['s3']['object']['key']
+        queue_url = os.environ['SQS_QUEUE_URL']
     
-    reader = csv.reader(lines)
-    for row in reader:
-        sqs.send_message(
-            QueueUrl=queue_url,
-            MessageBody=str(row)
-        )
-        time.sleep(random.randint(1, 5))
+        response = s3.get_object(Bucket=bucket, Key=csv_key)
+        lines = response['Body'].read().decode('ISO-8859-1').splitlines()
+        
+        reader = csv.reader(lines, delimiter=';')
+        next(reader)
+        for row in reader:
+            print(f'row: {row}')
+            message=';'.join(row)
+            sqs.send_message(
+                QueueUrl=queue_url,
+                MessageBody=message
+            )
+            print(f'message sent: {message}')
+        print('End')
+
+    except Exception as e:
+        print(f'e: {e}')
