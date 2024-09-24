79fe7c375945d57b12d1b0f6ce83768fceba65f3
Marcelo Pena <marcelo.pena@viaconsulting.com.br>
2024-09-24 04:07:24 -0300

    Update files

--- Atividade 08/script-consumer.py
+++ Atividade 08/script-consumer.py
@@ -1,8 +1,11 @@
 import json
-from kafka import KafkaProducer
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, col
+from pyspark.sql.functions import split, col, lpad
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+import os
+
+os.environ['HADOOP_HOME'] = 'C:/hadoop'
+os.environ['PATH'] += os.pathsep + 'C:/hadoop/bin'
 
 with open('credentials.json', 'r') as f:
     credentials = json.load(f)
@@ -14,68 +17,69 @@ kafka_server = credentials['kafka_server']
 topic = credentials['topic']
 
 spark = SparkSession.builder \
-    .appName("KafkaConsumer") \
-    .config("spark.sql.warehouse.dir", "file:///C:/temp") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1") \
-    .config("spark.jars.packages", "mysql:mysql-connector-java:8.0.30") \
+    .appName('KafkaConsumer') \
+    .config('spark.sql.warehouse.dir', 'file:///C:/tmp') \
+    .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,mysql:mysql-connector-java:8.0.30') \
+    .config('spark.driver.extraClassPath', 'C:/Users/mdeso/.ivy2/jars/mysql_mysql-connector-java-8.0.30.jar') \
+    .config('spark.hadoop.io.native.lib.available', 'false') \
+    .config("spark.hadoop.io.nativeio", "false") \
+    .config("spark.cleaner.referenceTracking.cleanCheckpoints", "false") \
     .getOrCreate()
 
 schema = StructType([
-    StructField("Ano", StringType(), True),
-    StructField("Trimestre", StringType(), True),
-    StructField("Categoria", StringType(), True),
-    StructField("Tipo", StringType(), True),
-    StructField("CNPJ", StringType(), True),  # "CNPJ IF" 
-    StructField("Instituicao", StringType(), True),
-    StructField("Indice", StringType(), True),
-    StructField("Reclamacoes_Procedentes", IntegerType(), True),
-    StructField("Reclamacoes_Outras", IntegerType(), True),
-    StructField("Reclamacoes_Nao_Reguladas", IntegerType(), True),
-    StructField("Total_Reclamacoes", IntegerType(), True),
-    StructField("Clientes_CCS_SCR", IntegerType(), True),
-    StructField("Clientes_CCS", IntegerType(), True),
-    StructField("Clientes_SCR", IntegerType(), True),
-    StructField("_14", StringType(), True)
+    StructField('Ano', StringType(), True),
+    StructField('Trimestre', StringType(), True),
+    StructField('Categoria', StringType(), True),
+    StructField('Tipo', StringType(), True),
+    StructField('CNPJ', StringType(), True),  # 'CNPJ IF' 
+    StructField('Instituicao', StringType(), True),
+    StructField('Indice', StringType(), True),
+    StructField('Reclamacoes_Procedentes', IntegerType(), True),
+    StructField('Reclamacoes_Outras', IntegerType(), True),
+    StructField('Reclamacoes_Nao_Reguladas', IntegerType(), True),
+    StructField('Total_Reclamacoes', IntegerType(), True),
+    StructField('Clientes_CCS_SCR', IntegerType(), True),
+    StructField('Clientes_CCS', IntegerType(), True),
+    StructField('Clientes_SCR', IntegerType(), True)
 ])
 
 df = spark \
     .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_server) \
-    .option("subscribe", topic) \
+    .format('kafka') \
+    .option('kafka.bootstrap.servers', kafka_server) \
+    .option('subscribe', topic) \
     .load()
 
-df_string = df.selectExpr("CAST(value AS STRING) as csv_value")
-df_split = df_string.withColumn("csv_cols", split(col("csv_value"), ";"))
+df_string = df.selectExpr('CAST(value AS STRING) as csv_value')
+df_split = df_string.withColumn('csv_cols', split(col('csv_value'), ';'))
 
-for i, column_name in enumerate(["Ano", "Trimestre", "Categoria", "Tipo", "CNPJ", "Instituicao", "Indice", 
-                                 "Reclamacoes_Procedentes", "Reclamacoes_Outras", "Reclamacoes_Nao_Reguladas",
-                                 "Total_Reclamacoes", "Clientes_CCS_SCR", "Clientes_CCS", "Clientes_SCR", "_14"]):
-    df_split = df_split.withColumn(column_name, col("csv_cols")[i])
+columns = ['Ano', 'Trimestre', 'Categoria', 'Tipo', 'CNPJ', 'Instituicao', 'Indice', 
+           'Reclamacoes_Procedentes', 'Reclamacoes_Outras', 'Reclamacoes_Nao_Reguladas',
+           'Total_Reclamacoes', 'Clientes_CCS_SCR', 'Clientes_CCS', 'Clientes_SCR']
 
-source_table_name = "trusted_banks"
+for i, column_name in enumerate(columns):
+    df_split = df_split.withColumn(column_name, col('csv_cols')[i])
 
-df_enriched = df_split.join(
-    spark.read.format("jdbc")
-    .option("url", url)
-    .option("dbtable", source_table_name)
-    .option("user", username)
-    .option("password", password)
-    .load(),
-    df_split.CNPJ == col(f'{source_table_name}.cnpj'),
-    how="left"
-)
+df_split = df_split.withColumn('CNPJ', lpad(col('CNPJ'), 8, '0'))
 
-query = df_enriched \
-    .writeStream \
-    .outputMode("append") \
-    .format("csv") \
-    .option("path", "file:///C:/temp/enriched_messages/") \
-    .option("checkpointLocation", "file:///C:/temp/checkpoints/") \
-    .trigger(processingTime='10 seconds') \
+df_banks = spark.read.format('jdbc') \
+    .option('url', url) \
+    .option('dbtable', '(SELECT cnpj, segment, financial_institution_name FROM eedb011.trusted_banks) as trusted_banks') \
+    .option('user', username) \
+    .option('password', password) \
+    .load()
+
+print(df_banks.show())
+
+df_joined = df_split.join(df_banks, df_split['CNPJ'] == df_banks['cnpj'], 'left')
+
+query = df_joined.writeStream \
+    .outputMode('append') \
+    .format('csv') \
+    .option('path', './enriched_messages/') \
+    .option('checkpointLocation', './checkpoints/') \
     .start()
 
-try:
-    query.awaitTermination()
-except Exception as e:
-    print(f"Error occurred: {e}")
+query.awaitTermination()
+
+spark.stop()
